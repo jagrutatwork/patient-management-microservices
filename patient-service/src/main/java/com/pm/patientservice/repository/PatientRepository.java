35fd4690012902cbb6ecba5a11155f2e96deb5b5
jagrutatwork <jagrutatwork@gmail.com>
2025-04-05 00:50:14 +0530

    Docker Image creation and Open API

--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.pm.patientservice.repository;
 
 import com.pm.patientservice.model.Patient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
@@ -8,5 +10,7 @@ import java.util.UUID;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
 
+    boolean existsByEmail(@NotNull @Email String email);
 
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
